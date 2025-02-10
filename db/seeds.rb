b43bc799824f67c2180285687309fd2ffe5e0e8c
Andrew W <andrew.winata@gmail.com>
2025-02-11 01:33:07 +0700

    Want to try making review on separate page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating restaurants..."
+Restaurant.create!(
+  name: "Dishoom",
+  address: "7 Boundary St, London E2 7JE",
+  phone_number: "+123 456 789",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Pizza East",
+  address: "56A Shoreditch High St, London E1 6PQ",
+  phone_number: "+123 456 789",
+  category: "italian",
+)
+
+Restaurant.create!(
+  name: "Sichuan Resto",
+  address: "North London",
+  phone_number: "+123 456 789",
+  category: "chinese",
+)
+
+Restaurant.create!(
+  name: "Ramen Resto",
+  address: "London CBD",
+  phone_number: "+123 456 789",
+  category: "japanese",
+)
+
+Restaurant.create!(
+  name: "Bistro",
+  address: "London South",
+  phone_number: "+123 456 789",
+  category: "french",
+)
